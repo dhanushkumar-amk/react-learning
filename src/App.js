cb9302e55bb19591c66b451fce51361483fe3b29
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-03 19:38:22 +0530

    basic changes in webpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import Hello from './Components/Hello';
-import Garage from './Components/Garage';
+import Basic from './Components/Basic';
+// import Hello from './Components/Hello';
+// import Garage from './Components/Garage';
 
 const App = () => {
   return (
     <div>
       {/* <Garage /> */}
       {/* <Hello /> */}
+
+      <Basic />
     </div>
   );
 };
---
+++ src/Components/Basic.jsx
@@ -0,0 +1,15 @@
+import React from 'react';const Basic = () => {
+  function handleNameChange() {
+    const names = ['Html', 'css', 'react', 'js'];
+    const int = Math.floor(Math.random() * 4);
+    return names[int];
+  }
+  return (
+    <div>
+      welcome to react
+      <p>Learn {handleNameChange()}</p>
+    </div>
+  );
+};
+
+export default Basic;
